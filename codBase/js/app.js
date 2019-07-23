e0ab2b71cc4edaa34664451961e66e0076a3ab4c
joaquin vezzoni <joacovs@gmail.com>
2019-07-23 18:37:30 -0300

    1.0

---
+++ codBase/js/app.js
@@ -0,0 +1,12 @@
+var Calculadora = {
+  display: document.getElementById('display'),
+  uno: document.getElementById('1'),
+  dos: document.getElementById('2'),
+  tres: document.getElementById('3'),
+  cuatro: document.getElementById('4'),
+  cinco: document.getElementById('5'),
+  seis: document.getElementById('6'),
+  siete: document.getElementById('7'),
+  ocho: document.getElementById('8'),
+  nueve: document.getElementById('9'),
+  cero: document.getElementById('0'),
