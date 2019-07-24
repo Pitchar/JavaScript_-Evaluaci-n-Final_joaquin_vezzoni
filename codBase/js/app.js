e0223a611fe6cce95de7e7e7e8c6c32c27a0610e
joaquin vezzoni <joacovs@gmail.com>
2019-07-24 19:17:16 -0300

    1.6

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -1,3 +1,12 @@
+var termino=0;
+var termino2=0;
+var termino3=0;
+var presionoSuma=false;
+var presionoResta=false;
+var presionoMultiplica=false;
+var presionoDivide=false;
+var resultado=0;
+
 var Calculadora = {
   display: document.getElementById('display'),
   cero: document.getElementById('0'),
@@ -21,85 +30,120 @@ var Calculadora = {
   init: function() {
   this.eventos()
 },
+//aumento y reducion de tamño de la teclas al precionar.
+  animarTecla: function(num){
+    var doc = document.getElementById(num);
+    doc.addEventListener('mousedown', function () {
+      doc.setAttribute("style", "transform:scale(0.95,0.95)");
+    })
+    doc.addEventListener('mouseup', function () {
+      doc.setAttribute("style", "transform:scale(1,1)");
+    })
+  },
+
   //agrgar todos los numeros.
   eventos: function() {
 
-  Calculadora.animarTecla("0")
+  Calculadora.animarTecla("0");
   this.cero.addEventListener("click", function() {
   Calculadora.mostrar("0")
   })
-  Calculadora.animarTecla("1")
+  Calculadora.animarTecla("1");
   this.uno.addEventListener("click", function(){
   Calculadora.mostrar("1")
   })
-  Calculadora.animarTecla("2")
+  Calculadora.animarTecla("2");
   this.dos.addEventListener("click", function(){
   Calculadora.mostrar("2")
   })
-  Calculadora.animarTecla("3")
+  Calculadora.animarTecla("3");
   this.tres.addEventListener("click", function() {
   Calculadora.mostrar("3")
   })
-  Calculadora.animarTecla("4")
+  Calculadora.animarTecla("4");
   this.cuatro.addEventListener("click", function() {
   Calculadora.mostrar("4")
   })
-  Calculadora.animarTecla("5")
+  Calculadora.animarTecla("5");
   this.cinco.addEventListener("click", function() {
   Calculadora.mostrar("5")
   })
-  Calculadora.animarTecla("6")
+  Calculadora.animarTecla("6");
   this.seis.addEventListener("click", function() {
   Calculadora.mostrar("6")
   })
-  Calculadora.animarTecla("7")
+  Calculadora.animarTecla("7");
   this.siete.addEventListener("click", function() {
   Calculadora.mostrar("7")
   })
-  Calculadora.animarTecla("8")
+  Calculadora.animarTecla("8");
   this.ocho.addEventListener("click", function() {
   Calculadora.mostrar("8")
   })
-  Calculadora.animarTecla("9")
+  Calculadora.animarTecla("9");
   this.nueve.addEventListener("click", function() {
-  Calculadora.mostrar("9")
+  Calculadora.mostrar("9");
   })
-  Calculadora.animarTecla("punto")
+  Calculadora.animarTecla("punto");
   this.punto.addEventListener("click", function() {
-  var valor = display.textContent
+  var valor = display.textContent;
   if (valor.indexOf(".") == -1) {
-  display.textContent += "."
+  display.textContent += ".";
   }
   })
-  Calculadora.animarTecla("sign")
+  Calculadora.animarTecla("sign");
   this.sign.addEventListener("click", function() {
   var valor = (display.textContent)*-1
   display.textContent=valor
-  })
+  }),
   //reiniciar display.
-  Calculadora.animarTecla("on")
+  Calculadora.animarTecla("on");
   this.on.addEventListener("click",function(){
   display.innerHTML=0
+  termino=0;
+  termino2=0;
+  termino3=0;
+  presionoSuma=false;
+  presionoResta=false;
+  presionoMultiplica=false;
+  presionoDivide=false;
+  resultado=0;
+  aux = 0
 })
   //operaciones matematicas.
-  Calculadora.animarTecla("mas")
+  Calculadora.animarTecla("mas");
   this.mas.addEventListener("click",function(){
-  Calculadora.sumar()
+  Calculadora.sumar();
 })
-  Calculadora.animarTecla("menos")
+  Calculadora.animarTecla("menos");
   this.mas.addEventListener("click",function(){
-  Calculadora.restar()
+  Calculadora.restar();
 })
-  Calculadora.animarTecla("por")
+  Calculadora.animarTecla("por");
   this.mas.addEventListener("click",function(){
-  Calculadora.multiplicar()
+  Calculadora.multiplicar();
 })
-  Calculadora.animarTecla("dividido")
+  Calculadora.animarTecla("dividido");
   this.mas.addEventListener("click",function(){
-  Calculadora.dividir()
-})
-
-
+  Calculadora.dividir();
+}),
+// condiciones para el igual
+Calculadora.animarTecla("igual");
+this.igual.addEventListener('click', function(){
+  if (presionoSuma==true){
+    Calculadora.resultadoSuma();
+  }
+  if (presionoResta==true){
+    Calculadora.resultadoResta();
+  }
+  if (presionoMultiplica==true){
+    Calculadora.resultadoMultiplica();
+  }
+  if (presionoDivide==true){
+    Calculadora.resultadoDivide();
+  }
+  })
+},
 
 }
-}
+Calculadora.init()
