241e2b0e1ecb57c825399e562c7e4970403f6e67
joaquin vezzoni <joacovs@gmail.com>
2019-07-24 17:58:32 -0300

    1.2

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -1,5 +1,6 @@
 var Calculadora = {
   display: document.getElementById('display'),
+  cero: document.getElementById('0'),
   uno: document.getElementById('1'),
   dos: document.getElementById('2'),
   tres: document.getElementById('3'),
@@ -9,4 +10,20 @@ var Calculadora = {
   siete: document.getElementById('7'),
   ocho: document.getElementById('8'),
   nueve: document.getElementById('9'),
-  cero: document.getElementById('0'),
+  punto: document.getElementById('punto'),
+  on: document.getElementById('on'),
+  mas: document.getElementById('mas'),
+  menos: document.getElementById('menos'),
+  por: document.getElementById('por'),
+  dividido: document.getElementById('dividido'),
+  igual: document.getElementById('igual'),
+  sign: document.getElementById('sign'),
+  init: function() {
+  this.eventos()
+},
+  eventos: function() {
+  //agrgar todos los numeros.
+  Calculadora.animarTecla("1");
+  this.uno.addEventListener('click', function() {
+    Calculadora.mostrar("1")
+  })
